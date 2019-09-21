0a81611bce2dac7e9140cefa783117eb22fc0dda
547h1J <jobayerhossain977@Gmail.com>
2019-09-21 19:04:45 +0600

    init

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// Function Activation
+$(document).ready(function () {
+    animationClass();
+    new WOW().init();
+    OwlCarouselFunction();
+    SlickMobileMenuFunction();
+});
+// Animation
+function animationClass() {
+    $("section").find("*").addClass("wow slideInUp").attr('data-wow-delay', '0.3s');
+}
+
+// Owl Carousel Function
+function OwlCarouselFunction() {
+    $('#home-sliders').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        autoplay: false,
+        autoplayHoverPause: true,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 3000,
+        navSpeed: 4000,
+        dotsSpeed: 4000,
+        dragEndSpeed: 1000,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        // responsive:{
+        //     0:{
+        //         items:1
+        //     },
+        //     600:{
+        //         items:3
+        //     },
+        //     1000:{
+        //         items:5
+        //     }
+        // },
+        items: 1
+    });
+}
+// Slick Mobile Menu Function
+function SlickMobileMenuFunction() {
+    $('#main-menu').slicknav({
+        label: '',
+        duration: 500,
+        prependTo: '#mobile-menu'
+    });
+}
